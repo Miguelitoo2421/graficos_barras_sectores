cc8d729812271d82d968d1d39ee55067728d6c25
Miguel Angel <migueel26@gmail.com>
2025-02-24 14:09:41 +0100

    generar graficos según mes

--- src/Clase7_Grupo2.py
+++ src/Clase7_Grupo2.py
@@ -2,7 +2,9 @@ import pandas as pd        # tratar los datos frames (tablas en python)
 import altair as alt       # elaborar gráficos
 import datapane as dp      # generar informes
 import os
-from PySide6.QtWidgets import QMainWindow, QComboBox
+
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import QMainWindow, QComboBox, QVBoxLayout, QWidget, QApplication
 
 fichero_csv = "prueba.csv"
 df = pd.read_csv(fichero_csv)
@@ -36,17 +38,40 @@ def generar_informe(mes):
     ruta_reporte = os.path.abspath("informe_ventas2.html")
     reporte.save(ruta_reporte)
 
-## CLASE VENTANA PRINCIPAL
+## CLASE VENTANA PRINCIPAL (NO ENTRA AL EXAMEN)
 class VentanaPrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Informe Ventas")
 
         self.selector_mes = QComboBox()
-        self.selector_mes.addItem(df["Mes"].unique())
+        self.selector_mes.addItems(df["Mes"].unique())
+        self.selector_mes.currentIndexChanged.connect(self.actualizar_reporte)
 
+        self.vista_web = QWebEngineView()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.selector_mes)
+        layout.addWidget(self.vista_web)
+
+        contenedor = QWidget()
+        contenedor.setLayout(layout)
+        self.setCentralWidget(contenedor)
+
+    def actualizar_reporte(self):
+        mes_seleccionado = self.selector_mes.currentText()
+        generar_informe(mes_seleccionado)
+        ruta_reporte = os.path.abspath("informe_ventas2.html")
+        if os.path.exists(ruta_reporte):
+            with open(ruta_reporte, "r", encoding='utf-8') as file:
+                html_content = file.read()
+                self.vista_web.setHtml(html_content)
 
 
 
 if __name__ == "__main__":
-    generar_informe("Enero")
+    #generar_informe("Enero") ------> esta llamada es la que va para el examen
+    app = QApplication([])
+    ventana = VentanaPrincipal()
+    ventana.show()
+    app.exec()
